8474444861651e41ab6e7ab5740ef1b26cee64aa
Priyosmita <priyosh08@gmail.com>
2023-04-27 17:22:12 +0530

    useHover

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,49 +1,46 @@
 import React, { useState } from "react";
 import { AiOutlineChrome } from "react-icons/ai";
+import useHover from "../hooks/useHover";
 
 const Profile = () => {
   const [tab, setTab] = useState("profile");
+  const [hoverRef, isHovered] = useHover();
 
   const handleChange = (value) => {
     setTab(value);
   };
+
+  const arr = ["Profile", "Socials", "Portfolio", "Resume"];
+
   return (
     <div className="w-[20%] mt-14 m-4 rounded-xl py-3 bg-gray-100">
-      <div className="w-2 h-[60%] rounded-r-lg hover:bg-black">
-        <div
-          value="profile"
-          onClick={() => handleChange("profile")}
-          className="h-10 absolute pl-9 flex items-center gap-1 text-sm font-bold text-gray-500 cursor-pointer relative"
-        >
-          <AiOutlineChrome />
-          Profile
-          <div className="absolute left-0 bg-black" />
-        </div>
-      </div>
-      <div
-        value="socials"
-        onClick={() => handleChange("socials")}
-        className="h-10 flex items-center gap-1 pl-2 text-sm font-bold text-gray-500 cursor-pointer"
-      >
-        <AiOutlineChrome />
-        Socials
-      </div>
-      <div
-        value="portfolio"
-        onClick={() => handleChange("portfolio")}
-        className="h-10 flex items-center gap-1 pl-2 text-sm font-bold text-gray-500 cursor-pointer"
-      >
-        <AiOutlineChrome />
-        Portfolio
-      </div>
-      <div
-        value="resume"
-        onClick={() => handleChange("resume")}
-        className="h-10 flex items-center gap-1 pl-2 text-sm font-bold text-gray-500 cursor-pointer"
-      >
-        <AiOutlineChrome />
-        Resume
-      </div>
+      {arr.map((item) => {
+        return (
+          <div key={item} ref={hoverRef}>
+            {isHovered ? (
+              <div
+                value="profile"
+                onClick={() => handleChange(item)}
+                className="h-10 pl-9 flex items-center gap-1 text-sm font-bold text-gray-500 cursor-pointer relative"
+              >
+                <AiOutlineChrome /> 😁
+                {item}
+                <div className="absolute w-2 h-full left-0 hover:bg-black" />
+              </div>
+            ) : (
+              <div
+                value="profile"
+                onClick={() => handleChange(item)}
+                className="h-10 pl-9 flex items-center gap-1 text-sm font-bold text-gray-500 cursor-pointer relative"
+              >
+                <AiOutlineChrome /> ☹️
+                {item}
+                <div className="absolute w-2 h-full left-0 hover:bg-black" />
+              </div>
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 };
